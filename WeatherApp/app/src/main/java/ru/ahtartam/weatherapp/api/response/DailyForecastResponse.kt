fdeff4d08c53446970094e40ab21050ace488ebd
vmikhailov <vmikhailov@ebookapplications.com>
2020-11-03 03:03:48 +0300

    removing unused

--- WeatherApp/app/src/main/java/ru/ahtartam/weatherapp/api/response/DailyForecastResponse.kt
+++ WeatherApp/app/src/main/java/ru/ahtartam/weatherapp/api/response/DailyForecastResponse.kt
@@ -7,17 +7,9 @@ import java.util.*
 
 @JsonClass(generateAdapter = true)
 data class DailyForecastResponse(
-    @field:Json(name = "current")
-    val current: CurrentGroup,
     @field:Json(name = "daily")
     val daily: List<DailyGroup>
 ) {
-    data class CurrentGroup (
-        @field:Json(name = "dt")
-        val date: Long,
-        @field:Json(name = "temp")
-        val temperature: Float
-    )
     data class DailyGroup (
         @field:Json(name = "dt")
         val date: Long,
@@ -30,16 +22,12 @@ data class DailyForecastResponse(
     )
 
     fun mapToListDailyForecast(cityId: Int): List<DailyForecast> {
-        return daily
-            .map {
-                it.date to it.temperature.day
-            }
-            .map {
-                DailyForecast(
-                    cityId = cityId,
-                    date = Date(it.first * 1000),
-                    temperature = it.second
-                )
-            }
+        return daily.map {
+            DailyForecast(
+                cityId = cityId,
+                date = Date(it.date * 1000),
+                temperature = it.temperature.day
+            )
+        }
     }
 }
